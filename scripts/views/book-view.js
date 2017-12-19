a2c442cc7604ecbb107c30daadff660a5915494a
Jordan <JordanLesaca@Gmail.Com>
2017-12-19 15:37:26 -0800

    book-view.js

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -3,16 +3,7 @@ var bookView = {};
 
 (function (module) {
     bookView.initIndexPage = () => {
-        var template = Handlebars.compile($(‘.book-data’).text());
 
-        app.Book.numWordsByAuthor().forEach(stat => $(‘.book-stats’).append(template(stat)));
-     
-        $(‘#blog-stats .articles’).text(app.Article.all.length);
-        $(‘#blog-stats .words’).text(app.Article.numWordsAll());
       };
     module.Book = Book;
 })(app);
-
-.hides()
-
-.show()
