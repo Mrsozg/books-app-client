4e4617647b8080c9deaaa1b625f55b8d2d01e25a
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-19 12:40:47 -0800

    installed npm package

--- scripts/models/books.js
+++ scripts/models/books.js
@@ -1,16 +1,20 @@
 'use strict';
 var app = app || {};
 
+
+//Define a static property on Book called all, and assign an empty array as it's value.
+//Define a static method on Book called loadAll which takes rows as an argument, and sorts rows by title, maps //over rows to create an array of Book instances, and then assigns the new array of Books to Book.all.
 (function (module) {
   function Book(rawDataObj) {
-  // REVIEW: In Lab 8, we explored a lot of new functionality going on here.
-  // Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object.
-  // However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode.
-  //As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object.
-  //One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context.
-  //That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function.
-  //As a result, we no longer have to pass in the optional "this" argument to forEach!
-    Object.keys(rawDataObj).forEach(key =>this[key] = rawDataObj[key]);
+
+    Object.keys(rawDataObj).forEach(key =>{this[key] = rawDataObj[key]},this);
   }
-  module.Article = Article;
+  Book.all = [];
+
+  Book.prototype.toHtml = function() {
+  var template = Handlebars.compile($('#book-list-template').text());
+
+  return template(this);
+};
+  module.Book = Book;
 })(app);
