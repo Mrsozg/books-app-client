c886e55cb8228afd3a8d955b855e957cf9b62987
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-21 10:17:18 -0800

    function structure is changed

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -5,18 +5,19 @@ var app = app || {};
 (function (module) {
   const bookView = {}
 
-  bookView.initIndexPage =>
+  bookView.initIndexPage = () => {
     $('.container').hide();
     $('.book-view').show();
     module.Book.all.map(book => $('#book-list').append(book.toHtml('book-list-template')));
     $('#book-count').append(module.Book.all.length);
+  } 
 
-  bookView.initDetailPage  (id, callback) =>
+  bookView.initDetailPage = (id, callback) =>{
     $('.container').hide();
     $('#detail-desc').empty();
     $('.detail-view').show();
     module.Book.all.map(book => $('#detail-desc').append(book.toHtml('book-desc-template')));
 
-
+  }
   module.bookView = bookView;
 })(app)
