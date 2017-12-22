718a083a8380de3e4545edf88fad3f9158526e15
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-22 11:19:05 -0800

    we can see inserted book on home page

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -2,6 +2,6 @@
 
 page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page('/books/:id', ctx => app.Book.fetchOne(ctx.params.id, app.bookView.initDetailPage));
-page('/allbook', () => app.Book.fetchAll(app.bookView.initIndexPage));
+//page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
 page ('/new', app.bookView.initAddBookPage);
 page();
