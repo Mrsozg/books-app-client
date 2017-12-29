b419cafa04aaac4a97fbc99a4764c40a3eb8f096
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-28 18:49:47 -0800

    user story 4 is done and compleeted lab13

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
-page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/'
+, (ctx, next) => app.Book.fetchAll(() => app.bookView.initIndexPage(ctx, next))
+, (ctx, next) => app.adminView.verify(ctx, next)
+);
 
 page('/books/:id'
     ,(ctx,next) => app.Book.fetchOne(ctx, () => app.bookView.initDetailPage(ctx, next)));
@@ -10,5 +13,7 @@ page ('/new', app.bookView.initAddBookPage);
 page('/books/:id/update' 
     , (ctx, next) => app.Book.fetchOne(ctx, next)
     , ctx => app.bookView.initUpdateFormPage(ctx))
+
+page('/admin', (ctx, next) => app.adminView.initAdminPage(ctx,next));    
 
 page();
